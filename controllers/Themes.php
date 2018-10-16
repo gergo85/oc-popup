26b7fa865ba84a71c282fae9caaaf70907d839fd
Szabó Gergő <gergo85@gmail.com>
2018-10-16 11:21:07 +0200

    Minor code improvements

--- controllers/Themes.php
+++ controllers/Themes.php
@@ -31,16 +31,9 @@ class Themes extends Controller
 
     public function onActivate()
     {
-        if (($checkedIds = post('checked')) && is_array($checkedIds) && count($checkedIds)) {
-            foreach ($checkedIds as $itemId) {
-                if (!$item = Item::where('status', 2)->whereId($itemId)) {
-                    continue;
-                }
-
-                $item->update(['status' => 1]);
-            }
-
-            Flash::success(Lang::get('indikator.popup::lang.flash.activate'));
+        if ($this->isSelected()) {
+            $this->changeStatus(post('checked'), 2, 1);
+            $this->setMessage('activate');
         }
 
         return $this->listRefresh();
@@ -48,16 +41,9 @@ class Themes extends Controller
 
     public function onDeactivate()
     {
-        if (($checkedIds = post('checked')) && is_array($checkedIds) && count($checkedIds)) {
-            foreach ($checkedIds as $itemId) {
-                if (!$item = Item::where('status', 1)->whereId($itemId)) {
-                    continue;
-                }
-
-                $item->update(['status' => 2]);
-            }
-
-            Flash::success(Lang::get('indikator.popup::lang.flash.deactivate'));
+        if ($this->isSelected()) {
+            $this->changeStatus(post('checked'), 1, 2);
+            $this->setMessage('deactivate');
         }
 
         return $this->listRefresh();
@@ -65,8 +51,8 @@ class Themes extends Controller
 
     public function onRemove()
     {
-        if (($checkedIds = post('checked')) && is_array($checkedIds) && count($checkedIds)) {
-            foreach ($checkedIds as $itemId) {
+        if ($this->isSelected()) {
+            foreach (post('checked') as $itemId) {
                 if (!$item = Item::whereId($itemId)) {
                     continue;
                 }
@@ -80,9 +66,41 @@ class Themes extends Controller
                 $item->delete();
             }
 
-            Flash::success(Lang::get('indikator.popup::lang.flash.remove'));
+            $this->setMessage('remove');
         }
 
         return $this->listRefresh();
+    }
+
+    /**
+     * @return bool
+     */
+    private function isSelected()
+    {
+        return ($checkedIds = post('checked')) && is_array($checkedIds) && count($checkedIds);
+    }
+
+    /**
+     * @param $action
+     */
+    private function setMessage($action)
+    {
+        Flash::success(Lang::get('indikator.popup::lang.flash.'.$action));
+    }
+
+    /**
+     * @param $post
+     * @param $from
+     * @param $to
+     */
+    private function changeStatus($post, $from, $to)
+    {
+        foreach ($post as $itemId) {
+            if (!$item = Item::where('status', $from)->whereId($itemId)) {
+                continue;
+            }
+
+            $item->update(['status' => $to]);
+        }
     }
 }
